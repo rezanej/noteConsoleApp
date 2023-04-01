04e036616946f3ad5a26e6275a2fbb57ca69bcbe
rezanejP <rezanej666@gmail.com>
2023-04-01 18:16:57 +0430

    Add project files.

---
+++ noteApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace noteApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        static void showTitles()
+        {
+
+        }
+        static void showNote()
+
+        {
+
+        }
+        static void addNote()
+        {
+
+        }
+        static string chooseTag()
+        {
+
+        }
+    }
+}
