904fd07c66b1a85f39a4ed38291e50f5de59505c
rezanejP <rezanej666@gmail.com>
2023-04-01 18:54:39 +0430

    adding mainMenu Function (it is not complete) . adding note class

--- noteApp/Program.cs
+++ noteApp/Program.cs
@@ -2,13 +2,31 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace noteApp
+namespace NoteAppConsole
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            
+        }
+        static void mainMenu()
+        {
+            Console.WriteLine("This program will manage notes");
+            do
+            {
+                try
+                {
+                    Console.WriteLine("enter the number of the menu and press enter:");
+                    int menuNum = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("please enter just number (like: 2 ) : ");
+                }
+            }
+            while (true);
         }
         static void showTitles()
         {
---
+++ noteApp/note.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NoteAppConsole
+{
+    public class note
+    {
+        public note(int id0 , string text0,string title0,string tag0,DateTime dateCreated0)
+        {
+            id = id0;
+            text = text0;
+            title = title0;
+            tag = tag0;
+            dateCreated = dateCreated0;
+        }
+
+       
+        private int id { get; }
+        private string text { get; set; }
+        private string title { get; set; }
+        private string tag { get; set; }
+        private DateTime dateCreated { get; }
+
+
+
+    }
+}
