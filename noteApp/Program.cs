9b696c820a3a8ee4e15bb03d44699403cedb5ce3
rezanejP <rezanej666@gmail.com>
2023-04-01 20:23:37 +0430

    making some functions and some connection to database

--- noteApp/Program.cs
+++ noteApp/Program.cs
@@ -8,7 +8,11 @@ namespace NoteAppConsole
     {
         static void Main(string[] args)
         {
-            
+           /* DateTime a = new DateTime(2010,5,2,12,5,3);
+            note g = new note(2, "dkfdgfgfgfhdkf", "test5", "tag0", a);
+            note.addnote(g);*/
+            mainMenu();
+            Console.ReadKey();
         }
         static void mainMenu()
         {
@@ -18,19 +22,61 @@ namespace NoteAppConsole
                 try
                 {
                     Console.WriteLine("enter the number of the menu and press enter:");
+                    Console.WriteLine("1-show notes (includes delete too)\n2-add note\n=>");
                     int menuNum = int.Parse(Console.ReadLine());
-                    break;
+                    if (menuNum >= 1 && menuNum <= 2)  // i know about == just maybe number of menu was
+                                                       // more like 6 part it be easiter two just change one number
+                    {
+                        switch (menuNum)
+                        {
+                            case 1:
+                                showNotesMenu();
+                                break;
+                        }
+
+                        break;
+                    } 
+                    else throw new Exception() ;
+                            
                 }
                 catch
                 {
-                    Console.WriteLine("please enter just number (like: 2 ) : ");
+                    Console.WriteLine("please enter just number and from (1 , 2) : =>");
                 }
             }
             while (true);
         }
+        static void showNotesMenu()
+        {
+            Console.WriteLine("for going to note enter number of it  (like 6):\n=>");
+            showTitles();
+            do
+            {
+                try
+                {
+                    int menuNum = int.Parse(Console.ReadLine());
+                    if (menuNum >= 1 && menuNum <= note.numberOfNotes)                          
+                    {
+                        
+                    } 
+                    else throw new Exception() ;
+                            
+                }
+                catch
+                {
+                    Console.WriteLine("please enter just number (like 6) : =>");
+                }
+            }
+            while (true);
+
+        }
         static void showTitles()
         {
-
+            Console.WriteLine("notes:");
+            foreach (string item in note.getTitles())
+            {
+                Console.WriteLine(item);
+            }
         }
         static void showNote()
 
@@ -43,7 +89,8 @@ namespace NoteAppConsole
         }
         static string chooseTag()
         {
-
+            return "";
         }
+        
     }
 }
--- noteApp/note.cs
+++ noteApp/note.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.Data.SQLite;
 namespace NoteAppConsole
 {
     public class note
     {
-        public note(int id0 , string text0,string title0,string tag0,DateTime dateCreated0)
+        public static int numberOfNotes;
+        public note(int id0, string text0, string title0, string tag0, DateTime dateCreated0)
         {
             id = id0;
             text = text0;
@@ -15,14 +16,79 @@ namespace NoteAppConsole
             dateCreated = dateCreated0;
         }
 
-       
+
         private int id { get; }
         private string text { get; set; }
         private string title { get; set; }
         private string tag { get; set; }
         private DateTime dateCreated { get; }
 
+        public static string[] getTitles()
+        {
+
+            List<string> data = new List<string>();
+            string connectionString = "uri=file:database.db";
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                string commandString = "select title from notes";
+
+                using (SQLiteCommand command = new SQLiteCommand(commandString, connection))
+                {
+                    SQLiteDataReader reader = command.ExecuteReader();
+                    int length = 0;
+                    while (reader.Read())
+                    {
+                        data.Add($"{reader["title"]}");
+                    }
+                }
+                connection.Close();
+            }
+            numberOfNotes=data.Count;
+            return data.ToArray();
+        }
+        public static void createTable()
+        {
+            string connectionString = "uri=file:database.db";
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                string commandString = "create table notes(id integer primary key AUTOINCREMENT,title string ,text0 TEXT,tag varchar,date0 DATETIME )";
+
+                using (SQLiteCommand command = new SQLiteCommand(commandString, connection))
+                {
+                    command.ExecuteNonQuery();
+
+                    
+                }
+                connection.Close();
+            }
+        }
+        public static bool addnote(note noteObject)
+        {
+            string[] titles = note.getTitles();
+            foreach (string item in titles)
+            {
+                if (noteObject.title==item)
+                {
+                    return false;
+                }
+            }
+            string connectionString = "uri=file:database.db";
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                string commandString = $"INSERT INTO NOTES (TITLE,TEXT0,TAG,DATE0) VALUES ('{noteObject.title}','{noteObject.text}','{noteObject.tag}','{DateTime.Now}')";
+
+                using (SQLiteCommand command = new SQLiteCommand(commandString, connection))
+                {
+                    command.ExecuteNonQuery();
 
 
+                }
+                connection.Close();
+            }
+            return true;
+        }
     }
 }
